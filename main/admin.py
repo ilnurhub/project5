dba309ba38272e27e700a63c3ac671ca59f9cd4d
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-12-24 20:47:59 +0300

    commit

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from main.models import Student
+from main.models import Student, Subject
+
 
 # Register your models here.
 # admin.site.register(Student)
@@ -9,3 +10,9 @@ class StudentAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'is_active',)
     list_filter = ('is_active',)
     search_fields = ('first_name', 'last_name',)
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'student')
+    list_filter = ('student',)
